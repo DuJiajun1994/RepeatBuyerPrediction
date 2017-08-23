370828576dcb1c9739d1864f053a6e513225c4ae
dujiajun <dujiajun1994@163.com>
2017-08-23 01:17:05 -0700

    modify logistic regression

--- lib/models/logistic_regression.py
+++ lib/models/logistic_regression.py
@@ -3,13 +3,16 @@ from __future__ import division
 from __future__ import print_function
 
 import tensorflow as tf
+from tensorflow.contrib import layers
+from tensorflow.contrib.layers.python.layers import layers as layers_lib
 
 
 def logistic_regression(inputs):
-    input_shape = inputs.get_shape().as_list()
-    data_length = input_shape[1]
+    regularizer = layers.l2_regularizer(scale=0.1)
     with tf.variable_scope('logistic_regression'):
-        w = tf.Variable(tf.zeros([data_length, 2]))
-        b = tf.Variable(tf.zeros([2]))
-        net = tf.matmul(inputs, w) + b
+        net = layers_lib.fully_connected(inputs,
+                                         num_outputs=2,
+                                         activation_fn=None,
+                                         weights_regularizer=regularizer,
+                                         biases_regularizer=regularizer)
     return net
