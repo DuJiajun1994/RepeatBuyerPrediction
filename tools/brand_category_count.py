f1e1018fc057807da2b5c0ef5a81f938d700ad13
dujiajun <dujiajun1994@163.com>
2017-08-26 23:14:19 -0700

    use DataFrame.groupby

--- tools/brand_category_count.py
+++ tools/brand_category_count.py
@@ -1,35 +1,12 @@
 import pandas as pd
 
 
-def add_item(dic, item):
-    if item in dic:
-        dic[item] += 1
-    else:
-        dic[item] = 1
+def group_and_count(df1, column_name):
+    df2 = pd.DataFrame({column_name: df1[column_name], 'number': df1['item_id']})
+    df3 = df2.groupby(column_name).count()
+    print(df3)
 
-
-def print_info(dic):
-    keys = list(dic.keys())
-    keys.sort()
-    for key in keys:
-        print('{}\t{}'.format(key, dic[key]))
 
 df = pd.read_csv('../data/user_log_format1.csv')
-
-categories = {}
-brands = {}
-for ix, row in df.iterrows():
-    if pd.notnull(row['cat_id']):
-        cat_id = int(row['cat_id'])
-        add_item(categories, cat_id)
-    if pd.notnull(row['brand_id']):
-        brand_id = int(row['brand_id'])
-        add_item(brands, brand_id)
-    if ix % 10000 == 0:
-        print('processing %d w' % int(ix / 10000))
-
-print('categories number: {}'.format(len(categories)))
-print_info(categories)
-print('brands number: {}'.format(len(brands)))
-print_info(brands)
-
+group_and_count(df, 'brand_id')
+group_and_count(df, 'cat_id')
